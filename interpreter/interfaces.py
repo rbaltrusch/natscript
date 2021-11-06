0a558a1896803cc197e616d7da0d461b2666852f
Richard Baltrusch <richard@baltrusch.net>
2021-11-06 14:06:01 +0100

    Add interfaces

---
+++ interpreter/interfaces.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov  6 13:44:51 2021
+
+@author: richa
+"""
+
+from __future__ import annotations
+
+from typing import Protocol, List, Dict, Optional, Any
+
+
+class Token(Protocol):
+    value: Any
+    RESOLUTION_ORDER: List[int]
+    EXPECTED_TOKENS: List[tuple]
+    VALUE_FACTORY: Optional[callable]
+    TOKEN_FACTORY: TokenFactory
+
+    def run(self, interpreter: Interpreter) -> None:
+        ...
+
+    def pop(self, tokens: List[Token]) -> None:
+        ...
+
+    def check_match(self, types: List[type]) -> bool:
+        ...
+
+
+class Variable(Protocol):
+    value: Any
+    name: str
+
+
+class Value(Protocol):
+    value: Any
+
+
+class TokenFactory(Protocol):
+    tokens: Dict[str, Token]
+    regex_tokens: Dict[str, Token]
+
+    def create_token(self, token: str) -> Token:
+        ...
+
+    @staticmethod
+    def create_variable(name: str) -> Variable:
+        ...
+
+    @staticmethod
+    def create_value(value: str) -> Value:
+        ...
+
+
+class Interpreter(Protocol):
+    def interpret(self, block: Block) -> None:
+        ...
+
+    def stack_pop(self) -> Any:
+        ...
+
+    def stack_append(self, value: Any) -> None:
+        ...
+
+    def check_variable(self, name: str) -> bool:
+        ...
+
+    def get_variable(self, name: str) -> Variable:
+        ...
+
+    def set_variable(self, name: str, value: Any) -> None:
+        ...
+
+
+class Block(Protocol):
+    def run(self, interpreter: Interpreter) -> None:
+        ...
