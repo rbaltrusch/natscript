d9d2d816edc94517edcf397d752f6c0369231f33
Richard Baltrusch <richard@baltrusch.net>
2022-04-12 23:16:38 +0200

    Fix tests

--- tests/smoke/examples_test.py
+++ tests/smoke/examples_test.py
@@ -7,17 +7,15 @@ Created on Tue Dec 14 21:40:44 2021
 
 import glob
 
-from interpret import interpret
+import interpret
 
 def test_examples():
     filepaths = glob.glob('../examples/**/*.nat', recursive=True)
     for filepath in filepaths:
         print(f'Running {filepath}...')
-        with open(filepath, 'r') as file:
-            file_contents = file.read()
 
         try:
-            interpret(file_contents)
+            interpret.interpret(filepath)
             passed = True
         except Exception as exc:
             raise exc
