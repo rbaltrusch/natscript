94ac4d60d9d2924fc0fa9bc063260a7da4232c1c
Richard Baltrusch <richard@baltrusch.net>
2022-04-15 17:11:18 +0200

    Account for bytecode compiler in tests

--- tests/smoke/examples_test.py
+++ tests/smoke/examples_test.py
@@ -5,11 +5,22 @@ Created on Tue Dec 14 21:40:44 2021
 @author: richa
 """
 
+import os
 import glob
 
 import interpret
 
 def test_examples():
+    _delete_compiled_files()
+    _run_test_() #without compiled files
+    _run_test_() #with compiled files
+
+def _delete_compiled_files():
+    compiled_filepaths = glob.glob('../examples/**/*.natc', recursive=True)
+    for filepath in compiled_filepaths:
+        os.unlink(filepath)
+
+def _run_test_():
     filepaths = glob.glob('../examples/**/*.nat', recursive=True)
     for filepath in filepaths:
         print(f'Running {filepath}...')
