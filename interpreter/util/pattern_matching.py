cc48d14cce41a99bc03b0c6b79a67955668323a0
Richard Baltrusch <richard@baltrusch.net>
2022-04-18 12:22:51 +0200

    Add docstrings and typehints

--- interpreter/util/pattern_matching.py
+++ interpreter/util/pattern_matching.py
@@ -1,24 +1,38 @@
 # -*- coding: utf-8 -*-
+"""
+This python module matches repeating patterns in a list of elements,
+which can be used e.g. to remove repeating parts from recursive stack traces.
+"""
 from __future__ import annotations
 
 from collections import defaultdict
 from dataclasses import dataclass
 from dataclasses import field
+from typing import DefaultDict
 from typing import Dict
+from typing import Hashable
 from typing import List
+
+DiffDict = Dict[Hashable, DefaultDict[int, List[int]]]
 
 
 @dataclass
 class Pattern:
+    """Pattern class, contains data for a repeating pattern"""
+
     start_index: int = 0
     length: int = 0
-    repetitions: int = 1
+    repetitions: int = 2
     valid: bool = field(default=True, repr=False)
 
     def check_matching_pattern(self, pattern: Pattern) -> bool:
+        """Returns True if pattern lengths match and the pattern start index is in range."""
         return self.length == pattern.length and pattern.start_index in self.range
 
-    def delay_start(self):
+    def delay_start(self) -> None:
+        """Decrements pattern repetitions by 1,
+        and sets valid to False if less than 2 repetitions.
+        """
         self.repetitions -= 1
         self.start_index += self.length
         if self.repetitions < 2:
@@ -26,19 +40,26 @@ class Pattern:
 
     @property
     def range(self) -> range:
+        """The index range covered by the repeating pattern"""
         return range(self.start_index, self.end)
 
     @property
     def end(self) -> int:
+        """The index at which the repeating pattern stops"""
         return self.start_index + self.length * self.repetitions
 
 
-def determine_distances_between_elements(elements):
-    indices = defaultdict(list)
+def determine_distances_between_patterns(elements: List[Hashable]) -> DiffDict:
+    """Returns a dict mapping of element to a mapping of {
+        length between same element occurence:
+        list of indices at starting indices at which element occured
+    }.
+    """
+    indices: DefaultDict[Hashable, List[int]] = defaultdict(list)
     for i, element in enumerate(elements):
         indices[element].append(i)
 
-    differences = {element: defaultdict(list) for element in elements}
+    differences: DiffDict = {element: defaultdict(list) for element in elements}
     for element, pattern in indices.items():
         if len(pattern) < 2:
             continue
@@ -49,7 +70,12 @@ def determine_distances_between_elements(elements):
     return differences
 
 
-def determine_patterns_in_distances(differences: Dict[int, Dict[int, List[int]]]):
+def determine_patterns_in_distances(differences: DiffDict) -> Dict[int, Pattern]:
+    """Returns a dictionary of starting index to a spotted repetition pattern from the
+    differences dict passed.
+
+    See determine_distances_between_elements for differences doc.
+    """
     spotted_patterns: Dict[int, Pattern] = {}
     for differences_ in differences.values():
         if not differences_:
@@ -58,7 +84,7 @@ def determine_patterns_in_distances(differences: Dict[int, Dict[int, List[int]]]
         for diff, start_indices in differences_.items():
             current_pattern = Pattern(length=0, repetitions=0)
             for start_index in start_indices:
-                new_pattern = Pattern(start_index, length=diff, repetitions=2)
+                new_pattern = Pattern(start_index, length=diff)
                 if current_pattern.check_matching_pattern(new_pattern):
                     current_pattern.repetitions += 1
                     continue
@@ -72,7 +98,8 @@ def determine_patterns_in_distances(differences: Dict[int, Dict[int, List[int]]]
     return spotted_patterns
 
 
-def _determine_valid_patterns(spotted_patterns):
+def _determine_valid_patterns(spotted_patterns: Dict[int, Pattern]) -> List[Pattern]:
+    """Filters out invalid patterns and duplicates"""
     for pattern in sorted(spotted_patterns.values(), key=lambda x: x.start_index):
         _check_validity(pattern, spotted_patterns)
     patterns = [pattern for pattern in spotted_patterns.values() if pattern.valid]
@@ -80,6 +107,10 @@ def _determine_valid_patterns(spotted_patterns):
 
 
 def _check_validity(pattern: Pattern, spotted_patterns: Dict[int, Pattern]):
+    """Recursively filters out invalid and duplicate patterns. When patterns could match,
+    but don't because of their respective starting indices, it tries to start matching the
+    earliest pattern from a later point.
+    """
     if pattern.repetitions < 2 or not pattern.valid:
         pattern.valid = False
         return
@@ -88,6 +119,9 @@ def _check_validity(pattern: Pattern, spotted_patterns: Dict[int, Pattern]):
     end = pattern.start_index + pattern.length
     for i in range(start, end):
         matching_pattern = spotted_patterns.get(i)
+
+        # patterns dont necessarily match at each others first starting index
+        # e.g. [6, 8, 6, 7, 6, 7] has the 6 pattern start at 0 and the 7 pattern start at 4
         if matching_pattern is None:
             pattern.delay_start()
             _check_validity(pattern, spotted_patterns)
@@ -100,8 +134,11 @@ def _check_validity(pattern: Pattern, spotted_patterns: Dict[int, Pattern]):
         pattern.repetitions = min(pattern.repetitions, matching_pattern.repetitions)
 
 
-def determine_patterns(elements):
-    distances = determine_distances_between_elements(elements)
+def determine_patterns(elements: List[Hashable]) -> List[Pattern]:
+    """Returns a list of Pattern objects determined from repeating patterns
+    in the specified elements.
+    """
+    distances = determine_distances_between_patterns(elements)
     patterns = determine_patterns_in_distances(distances)
     valid_patterns = _determine_valid_patterns(patterns)
     return valid_patterns
