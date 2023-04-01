081ca30237cbb7534d7a57424104f539abf5c8c3
Richard Baltrusch <richard@baltrusch.net>
2023-04-01 13:19:01 +0200

    Format code

--- tests/smoke/examples_test.py
+++ tests/smoke/examples_test.py
@@ -5,28 +5,31 @@ Created on Tue Dec 14 21:40:44 2021
 @author: richa
 """
 
-import os
 import glob
+import os
 
 from interpreter import interpret
 from interpreter.tokens_ import compiler
 
+
 def test_examples():
     _delete_compiled_files()
-    _run_test_() #without compiled files
-    _run_test_() #with compiled files
+    _run_test_()  # without compiled files
+    _run_test_()  # with compiled files
+
 
 def _delete_compiled_files():
-    compiled_filepaths = glob.glob('../examples/**/*.natc', recursive=True)
+    compiled_filepaths = glob.glob("../examples/**/*.natc", recursive=True)
     for filepath in compiled_filepaths:
         os.unlink(filepath)
 
+
 def _run_test_():
-    filepaths = glob.glob('../examples/**/*.nat', recursive=True)
+    filepaths = glob.glob("../examples/**/*.nat", recursive=True)
     original_dir = os.getcwd()
     compiler_ = compiler.PickleCompiler()
     for filepath in filepaths:
-        print(f'Running {filepath}...')
+        print(f"Running {filepath}...")
 
         os.chdir(os.path.dirname(filepath))
         filename = os.path.basename(filepath)
@@ -42,7 +45,7 @@ def _run_test_():
             passed = True
         except Exception as exc:
             raise exc
-        assert passed, f'{filepath} could not be interpreted!'
+        assert passed, f"{filepath} could not be interpreted!"
 
         os.chdir(original_dir)
-        print(f'{filepath} passed.')
+        print(f"{filepath} passed.")
