d1da75d309d18832bd4fa503388423b39cc1b379
Richard Baltrusch <richard@baltrusch.net>
2023-04-01 13:19:46 +0200

    Optimize interpreter stack and variable access

--- interpreter/internal/interpreter.py
+++ interpreter/internal/interpreter.py
@@ -19,6 +19,9 @@ class Interpreter:
     def __init__(self):
         self._stacks: List[List[Value]] = [[]]
         self._variables: List[Dict[str, Variable]] = [{}]
+        self._current_stack_pop = self._stacks[-1].pop.__call__
+        self._current_stack_append = self._stacks[-1].append.__call__
+        self._current_variables: Dict[str, Variable] = self._variables[-1]
 
     def run(self, token: Token) -> None:
         """Runs the current token"""
@@ -43,11 +46,17 @@ class Interpreter:
         self._stacks.append([])
         globals_dict = self._variables[0].copy()
         self._variables.append(globals_dict)
+        self._current_stack_pop = self._stacks[-1].pop.__call__
+        self._current_stack_append = self._stacks[-1].append.__call__
+        self._current_variables = self._variables[-1]
 
     def remove_stack(self) -> None:
         """Removes the last stack from the stack of stacks"""
         self._stacks.pop()
         self._variables.pop()
+        self._current_stack_pop = self._stacks[-1].pop.__call__
+        self._current_stack_append = self._stacks[-1].append.__call__
+        self._current_variables = self._variables[-1]
 
     def stack_pop(self) -> Value:
         """Pops and returns the last value on the stack.
@@ -55,17 +64,17 @@ class Interpreter:
         Raises an EmptyStackError if the stack is empty.
         """
         try:
-            return self._stacks[-1].pop()
+            return self._current_stack_pop()  # pylint: disable=not-callable
         except IndexError:
             raise exceptions.EmptyStackError() from None
 
     def stack_append(self, value: Value) -> None:
         """Appends the passed Value to the stack"""
-        self._stacks[-1].append(value)
+        self._current_stack_append(value)  # pylint: disable=not-callable
 
     def check_variable(self, name: str) -> bool:
         """Returns True if the passed name is in the variables list"""
-        return name in self._variables[-1]
+        return name in self._current_variables
 
     def get_variable(self, name: str) -> Variable:
         """Returns a Variable if it can be looked up by name.
@@ -73,14 +82,14 @@ class Interpreter:
         Raises an UndefinedVariableException if variable cannot be found.
         """
         try:
-            return self._variables[-1][name]
+            return self._current_variables[name]
         except KeyError:
             raise exceptions.UndefinedVariableException(name) from None
 
     def set_variable(self, name: str, variable: Variable) -> None:
         """Sets the value of the variable identified by name to the specified variable."""
-        self._variables[-1][name] = variable
+        self._current_variables[name] = variable
 
     def remove_variable(self, name: str) -> None:
         """Removes the variable identified by name from the current variable scope"""
-        self._variables[-1].pop(name)
+        self._current_variables.pop(name)
