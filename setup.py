6c39b107f34e83a717bdf41ce488fa0acc1b2400
Richard Baltrusch <richard@baltrusch.net>
2023-04-07 16:59:31 +0200

    Expose VERSION constant in setup.py

--- setup.py
+++ setup.py
@@ -1,40 +1,47 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""Packaging script"""
 
 from pathlib import Path
-import setuptools
 
-project_dir = Path(__file__).parent
+VERSION = "0.1.0"
 
-setuptools.setup(
-    name="interpreter",
-    version="1.0.0",
-    description="Natscript interpreter",
-    long_description=project_dir.joinpath("README.md").read_text(encoding="utf-8"),
-    long_description_content_type="text/markdown",
-    keywords=["python"],
-    author="Richard Baltrusch",
-    url="https://github.com/rbaltrusch/python_interpreter",
-    packages=setuptools.find_packages("."),
-    package_dir={"": "."},
-    python_requires=">=3.8",
-    include_package_data=True,
-    package_data={"interpreter": ["py.typed"]}, # for mypy
-    # This is a trick to avoid duplicating dependencies between both setup.py and requirements.txt.
-    # requirements.txt must be included in MANIFEST.in for this to work.
-    install_requires=project_dir.joinpath("requirements.txt").read_text().split("\n"),
-    zip_safe=False,
-    license="MIT",
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Typing :: Typed",
-    ],
-)
+if __name__ == "__main__":
+    project_dir = Path(__file__).parent
+
+    import setuptools
+
+    setuptools.setup(
+        name="interpreter",
+        version=VERSION,
+        description="Natscript interpreter",
+        long_description=project_dir.joinpath("README.md").read_text(encoding="utf-8"),
+        long_description_content_type="text/markdown",
+        keywords=["python"],
+        author="Richard Baltrusch",
+        url="https://github.com/rbaltrusch/python_interpreter",
+        packages=setuptools.find_packages("."),
+        package_dir={"": "."},
+        python_requires=">=3.8",
+        include_package_data=True,
+        package_data={"interpreter": ["py.typed"]},  # for mypy
+        # This is a trick to avoid duplicating dependencies in both setup.py and requirements.txt.
+        # requirements.txt must be included in MANIFEST.in for this to work.
+        install_requires=project_dir.joinpath("requirements.txt")
+        .read_text()
+        .split("\n"),
+        zip_safe=False,
+        license="MIT",
+        classifiers=[
+            "Development Status :: 4 - Beta",
+            "Intended Audience :: Developers",
+            "License :: OSI Approved :: MIT License",
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3 :: Only",
+            "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: 3.9",
+            "Programming Language :: Python :: 3.10",
+            "Typing :: Typed",
+        ],
+    )
