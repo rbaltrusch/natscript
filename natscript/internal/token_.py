a5d64f7d75276f73dce6e1219fee1a24a79c9c68
Richard Baltrusch <richard@baltrusch.net>
2024-06-23 14:42:06 +0200

    Fix iterable value check
    This removes the wrong assumption that if TokenFactory.create_value throws a TypeError for a specific value, that that value must be an interable. Instead, it is now explicitly checked if the value can be iterated using iter(value), and returning a normal value if that errors out.

--- natscript/internal/token_.py
+++ natscript/internal/token_.py
@@ -59,9 +59,10 @@ class TokenFactory:
             return self.create_none_value()
 
         try:
-            return self.create_value(value)
-        except TypeError:
+            iter(value)
             return self.create_iterable_value(value)
+        except TypeError:
+            return self.create_value(value)
 
     def create_value(self, value: Hashable) -> tokenvalue.Value:
         """Returns a new Value object if value cannot be found in the value cache,
